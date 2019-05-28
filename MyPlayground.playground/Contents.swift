a95aa4d9043be44d81156eb271d95366e5957665
rosen777 <toshev@usc.edu>
2019-05-28 01:16:42 +0100

    A new playground with string interpolation with \() added

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+var favoriteWord = "mercurial"
+var favoriteColor = "blue"
+var favoriteMusician = "Nirvana"
+let color = "blue"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+print("I'm printing a string in Swift!")
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+print("The sky is \(color)")
